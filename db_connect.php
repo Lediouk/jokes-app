1f0982933876a5f57b812be807463582056d696e
Dioukou <126500344+Lediouk@users.noreply.github.com>
2024-08-31 18:00:16 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -10,7 +10,19 @@ $username = "ahbddshdku"; // Generated username
 $user_pass = "#$###kdsdaSDAS"; // Replace with the actual password
 $database_in_use = "jokeys-database"; // Name of your database
 
+// Path to SSL certificate file
+$ssl_cert = '/home/site/wwwroot/certs/BaltimoreCyberTrustRoot.crt.pem'; // Update this path
+
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use, $port);
+
+// Check if SSL certificates are needed
+if ($mysqli->connect_error) {
+    die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+}
+
+// Set SSL options
+$mysqli->ssl_set(NULL, NULL, $ssl_cert, NULL, NULL);
+$mysqli->real_connect($host, $username, $user_pass, $database_in_use, $port, NULL, MYSQLI_CLIENT_SSL);
 
 if ($mysqli->connect_error) {
     die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
