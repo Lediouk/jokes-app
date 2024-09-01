01ac203249e4bbd7efb2e3e95b0830fbea019d5f
Dioukou <126500344+Lediouk@users.noreply.github.com>
2024-08-31 17:36:50 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,18 +3,18 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// modify these settings according to the account on your database server.
-$host = "localhost";
-$port = "3306";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "jokesdb";
+// Modify these settings according to the account on your Azure MySQL server.
+$host = "jokeys-server.mysql.database.azure.com"; // Full server name with domain
+$port = "3306"; // Default MySQL port
+$username = "ahbddshdku"; // Generated username
+$user_pass = "your_password"; // Replace with the actual password
+$database_in_use = "jokeys-database"; // Name of your database
 
+$mysqli = new mysqli($host, $username, $user_pass, $database_in_use, $port);
 
-$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
 if ($mysqli->connect_error) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
 }
-echo $mysqli->host_info . "<br>";
 
+echo $mysqli->host_info . "<br>";
 ?>
