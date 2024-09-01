04a9de4078ed5d13d1e9bf41c5bbdde96b7b41a0
Dioukou <126500344+Lediouk@users.noreply.github.com>
2024-08-31 18:11:39 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,19 +3,18 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// Modify these settings according to the account on your Azure MySQL server.
-$host = "jokeys-server.mysql.database.azure.com"; // Full server name with domain
-$port = "3306"; // Default MySQL port
-$username = "ahbddshdku"; // Generated username
-$user_pass = "#$###kdsdaSDAS"; // Replace with the actual password
-$database_in_use = "jokeys-database"; // Name of your database
+// Fetch environment variables
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$username = getenv('DB_USER');
+$user_pass = getenv('DB_PASS');
+$database_in_use = getenv('DB_NAME');
 
 // Path to SSL certificate file
-$ssl_cert = '/home/site/wwwroot/certs/BaltimoreCyberTrustRoot.crt.pem'; // Update this path
+$ssl_cert = 'C:\\MAMP\\htdocs\\jokes-part2\\certs\\BaltimoreCyberTrustRoot.crt.pem'; // Update this path
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use, $port);
 
-// Check if SSL certificates are needed
 if ($mysqli->connect_error) {
     die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
 }
